b597e37795da708ecfe69d8407ac50d0762a6044
Ever Carvajal <62713105+Alejandro-C@users.noreply.github.com>
2020-08-11 21:10:26 -0500

    validacion para no repetir categorias

--- pm_administrador-de-productos/js/formCategoria.js
+++ pm_administrador-de-productos/js/formCategoria.js
@@ -1,23 +1,25 @@
 document.getElementById('formCategoria').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let categorias = [];
-    let id = 0;
+    let categorias = categoriasGuardadas();
+    let id = categorias.length;
 
     // OBTENIEDO LOS VALORES DEL FORMULARIO
     //el objeto FormData se rellenará con las claves/valores actuales del formulario mediante la propiedad name de cada elemento para las claves y su valor enviado para los valores.
     let datosFormulario = new FormData(document.getElementById('formCategoria'));
     // El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.
-    let cateoria = Object.fromEntries(datosFormulario);
+    let categoria = Object.fromEntries(datosFormulario);
+
+    // VALIDANDO SI YA EXISTE LA CATEGORIA QUE SE QUIERE CREAR
+    if (existeCategoria(categoria)) return;
 
     // AÑADIENDOLE OTROS ATRIBUTOS
-    cateoria.id = id++;
-    cateoria.relacionada = false;
+    categoria.id = id;
+    categoria.relacionada = false;
+    categoria.editar = false;
 
     // GUARDANDO LA NUEVA CATEGORIA EN EL ARRAY CATEGORIAS
-    categorias.push(cateoria);
-
-    console.log(categorias);
+    categorias.push(categoria);
 
     // GUARDANDO EN EL LOCALSTORAGE LAS CATEGORIAS
     localStorage.setItem('categorias', JSON.stringify(categorias));
--- pm_administrador-de-productos/js/funciones.js
+++ pm_administrador-de-productos/js/funciones.js
@@ -1,1 +1,41 @@
-function mostrarRegistros() { document.getElementById('sectionTabla').style.display = 'none'; }
+function mostrarRegistros() {
+    // seleccionando el elemento que contiene la tabla con los registros
+    document.getElementById('sectionTabla').style.display = 'none';
+}
+
+function categoriasGuardadas() {
+    let categorias = [];
+
+    // validando si existen categorias en el localStorage 
+    if (localStorage.getItem('categorias')) {
+        // llenando el array categorias con las categorias guardadas en el localStorage
+        categorias = JSON.parse(localStorage.getItem('categorias'));
+    }
+
+    return categorias;
+}
+
+function existeCategoria(categoria) {
+    // validando si hay categorias registradas
+    if (localStorage.getItem('categorias')) {
+        let categorias = JSON.parse(localStorage.getItem('categorias'));
+
+        // validando los posibles casos para no registrar la categoria
+        for (let i = 0; i < categorias.length; i++) {
+            if (categorias[i].codigo == categoria.codigo && categorias[i].descripcion == categoria.descripcion) {
+                alert('Esta categoria ya se encuentra registrada');
+                return true;
+            } else if (categorias[i].codigo == categoria.codigo) {
+                alert('El codigo ya esta en uso');
+                return true;
+            } else if (categorias[i].descripcion == categoria.descripcion) {
+                alert('La descripcion ya se encuentra en uso');
+                return true;
+            }
+        }
+
+
+    }
+
+    return false;
+}
