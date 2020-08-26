96eca63e28ab1f993499416e15f90e641b4f64f6
Ever Carvajal <62713105+Alejandro-C@users.noreply.github.com>
2020-08-26 09:24:50 -0500

    se hizo las validaciones para productos

--- pm_administrador-de-productos/js/formProducto.js
+++ pm_administrador-de-productos/js/formProducto.js
@@ -1,5 +1,8 @@
-if (!localStorage.getItem('categorias')) {
-    alert('Tienes que tener almenos una categoria creada para crear un producto');
+if (!localStorage.getItem('categorias') || JSON.parse(localStorage.getItem('categorias')).length < 4) {
+    let numeroCategorias = JSON.parse(localStorage.getItem('categorias')).length;
+
+    alert(`Tienes que tener almenos cuatro categorias creadas para crear un producto usted tiene: ${numeroCategorias}`);
+
     location.href = 'categorias.html';
 } else {
     llenarSelectCategorias();
@@ -7,5 +10,13 @@ if (!localStorage.getItem('categorias')) {
 
 document.getElementById('formProducto').addEventListener('submit', (e) => {
     e.preventDefault();
+
+    let datosFormulario = new FormData(document.getElementById('formProducto'));
+    let producto = Object.fromEntries(datosFormulario);
+
+    if (!producto.categoria) return alert("El campo 'categoria' es obligatorio");
+
+    console.log(producto);
+
     console.log('guardado');
 });
